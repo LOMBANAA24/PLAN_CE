926c3f18ab30d4fd5e4bda064ef76ef45e063c21
Cristian Lombana B <cridlombana1@soy.sena.edu.co>
2024-06-03 14:55:34 -0500

    Añadieron archivos a la carpeta navigations

---
+++ FrontEnd/MotosCE/src/Navigations/AppNavigation.jsx
@@ -0,0 +1,24 @@
+// src/navigations/AppNavigation.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabNavigation from './BottomTabNavigation';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName="Main"
+      >
+        <Stack.Screen name="Main" component={BottomTabNavigation} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
