86b8c1e23a22d36c3f63f84d4281083262034288
Cristian Lombana B <cridlombana1@soy.sena.edu.co>
2024-06-03 15:01:11 -0500

    Añadieron archivos a la carpeta screens

---
+++ FrontEnd/MotosCE/src/Screens/Main/GarageScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GarageScreen = () => {
+  return (
+    <View>
+      <Text>GarageScreen</Text>
+    </View>
+  )
+}
+
+export default GarageScreen
---
+++ FrontEnd/MotosCE/src/Screens/Main/MotorcycleScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MotorcycleScreen = () => {
+  return (
+    <View>
+      <Text>MotorcycleScreen</Text>
+    </View>
+  )
+}
+
+export default MotorcycleScreen
