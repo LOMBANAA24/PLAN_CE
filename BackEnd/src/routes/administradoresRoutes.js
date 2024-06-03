b24069122a37412f358578f68c65be4420865e11
Cristian Lombana B <cridlombana1@soy.sena.edu.co>
2024-06-03 12:16:12 -0500

    Cambios en la sub rama routes

---
+++ BackEnd/src/routes/administradoresRoutes.js
@@ -0,0 +1,12 @@
+// /routes/administradoresRoutes.js
+
+const administradoresController = require('../controllers/administradoresController');
+const verificarToken = require('./path_to/verificarToken');
+
+module.exports = (app) => {
+    app.post('/api/administradores/create', administradoresController.register);
+    app.post('/api/administradores/login', administradoresController.login);
+    app.get('/api/administradores/:id', verificarToken, administradoresController.getById);
+    app.put('/api/administradores/:id', verificarToken, administradoresController.update);
+    app.delete('/api/administradores/:id', verificarToken, administradoresController.delete);
+};
