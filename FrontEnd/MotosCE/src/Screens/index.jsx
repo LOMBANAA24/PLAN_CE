90a0adf0536e344ea885c4a70dafc4fb98ef68bd
Cristian Lombana B <cridlombana1@soy.sena.edu.co>
2024-06-03 15:00:49 -0500

    Añadieron archivos a la carpeta screens

---
+++ FrontEnd/MotosCE/src/Screens/index.jsx
@@ -0,0 +1,19 @@
+import HomeScreen from "./Main/HomeScreen";
+import AgenciesScreen from "./Details/AgenciesScreen";
+import CitiesScreen from "./Details/CitiesScreen";
+import GarageScreen from "./Main/GarageScreen";
+import MotorcycleScreen from "./Main/MotorcycleScreen";
+import Search from "./Details/Search";
+import ClientScreen from "./Main/ClientScreen";
+import ReservationScreen from "./Main/ReservationScreen";
+
+export {
+    HomeScreen,
+    AgenciesScreen,
+    CitiesScreen,
+    GarageScreen,
+    MotorcycleScreen,
+    Search,
+    ClientScreen,
+    ReservationScreen
+}
