a6a689bafa51cc073932b102cc45e96533ad84ae
Cristian Lombana B <cridlombana1@soy.sena.edu.co>
2024-06-03 14:48:36 -0500

    Se agregaron archivos en mi carpeta de constantes

---
+++ FrontEnd/MotosCE/src/constans/icons.jsx
@@ -0,0 +1,35 @@
+import home from '../assets/icons/home.png';
+import homeOutline from '../assets/icons/homeOutline.png';
+import office from '../assets/icons/office.png';
+import officeOutline from '../assets/icons/officeOutline.png';
+import city from '../assets/icons/city.png';
+import cityOutline from '../assets/icons/cityOutline.png';
+import garage from '../assets/icons/garage.png';
+import garageOutline from '../assets/icons/garageOutline.png';
+import motorcycle from '../assets/icons/motorcycle.png';
+import motorcycleOutline from '../assets/icons/motorcycleOutline.png';
+import search from '../assets/icons/search.png';
+import searchOutline from '../assets/icons/searchOutline.png';
+import user from '../assets/icons/user.png';
+import userOutline from '../assets/icons/userOutline.png';
+import calendar from '../assets/icons/calendar.png';
+import calendarOutline from '../assets/icons/calendarOutline.png';
+
+export default {
+    home,
+    homeOutline,
+    office,
+    officeOutline,
+    city,
+    cityOutline,
+    garage,
+    garageOutline,
+    motorcycle,
+    motorcycleOutline,
+    search,
+    searchOutline,
+    user,
+    userOutline,
+    calendar,
+    calendarOutline
+}
